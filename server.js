df803eafcdb695f7a8b4117619f681796c08aeec
kasp444 <kasparyss321@gmail.com>
2022-08-17 14:43:27 +0300

    ..

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
 
-// process.on("uncaughtException", (err) => {
-//   console.log(err.name, err.message);
-//   console.log("UNCAUGHT EXCEPTION, Shutting down");
-//   process.exit(1);
-// });
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION, Shutting down");
+  process.exit(1);
+});
 
 dotenv.config({ path: "./config.env" });
 
@@ -31,10 +31,10 @@ const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// process.on("unhandledRejection", (err) => {
-//   console.log(err.name, err.message);
-//   console.log("UNHANDLED REJECTION, Shutting down");
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION, Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
